d6f8c19537e2a221190571f6a30844d9e0b713f5
Oleksii Nosov <o.nosov@turbogames.io>
2025-04-19 17:47:13 +0300

    main

--- shippers.sql
+++ shippers.sql
@@ -1,11 +1,11 @@
-CREATE TABLE shippers (
+CREATE TABLE Shippers (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     phone VARCHAR(20)
 );
 
 -- Вставка записів
-INSERT INTO shippers (id, name, phone) VALUES
+INSERT INTO Shippers (id, name, phone) VALUES
 (1, 'Speedy Express', '(503) 555-9831'),
 (2, 'United Package', '(503) 555-3199'),
 (3, 'Federal Shipping', '(503) 555-9931');
